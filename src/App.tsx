06f50f6338fb8f82a89ec4afb048cb7ec8917acf
ArtemFedorchuk <fedorchuk.zver@gmail.com>
2023-08-26 23:24:47 +0300

    init mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from "./components/counter";
+import {Posts} from "./components/posts";
 
 function App() {
   return (
@@ -8,7 +10,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload
+          <Counter/>
+          <br/>
+          <Posts/>
         </p>
         <a
           className="App-link"
